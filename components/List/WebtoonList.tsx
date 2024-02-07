5a2965c1d73d4fc7860d5aa03f2ce9897e4ea7d2
yichangmin <dogfeet.holiday@gmail.com>
2024-02-08 00:16:26 +0900

    컴포넌트 분리

--- components/List/WebtoonList.tsx
+++ components/List/WebtoonList.tsx
@@ -1,6 +1,7 @@
 import { StarIcon, HStack, Icon, Image, Text, VStack } from '@gluestack-ui/themed';
 import { Webtoon } from '../../types';
 import { useQuery } from '@tanstack/react-query';
+import MediumCard from '../Card/MediumCard';
 
 interface WebtoonResponse {
   webtoons: Webtoon[];
@@ -19,40 +20,7 @@ const WebtoonList = () => {
   return (
     <VStack gap='$2' px='$3' mt='$5'>
       {data &&
-        data.webtoons.map((webtoon) => (
-          <HStack borderWidth='$1' borderColor='$backgroundLight700'>
-            <Image
-              bg='$backgroundLight700'
-              alt={webtoon.title}
-              size='xl'
-              source={{
-                uri: webtoon.img,
-              }}
-            />
-            <VStack justifyContent='center' pl='$5' gap='$1'>
-              <Text color='$blueGray50' size='md' bold={true}>
-                {webtoon.title}
-              </Text>
-              <Text color='$blueGray300' size='sm' bold={true}>
-                {webtoon.author}
-              </Text>
-              <HStack gap='$1'>
-                <Icon
-                  as={StarIcon}
-                  size='sm'
-                  color='$yellow500'
-                  fill='$yellow300'
-                  mt='$0.5'
-                  ml='$0.5'
-                  $dark-color='$primary300'
-                />
-                <Text color='$blueGray300' size='sm' bold={true}>
-                  {webtoon.fanCount}
-                </Text>
-              </HStack>
-            </VStack>
-          </HStack>
-        ))}
+        data.webtoons.map((webtoon) => <MediumCard key={webtoon.webtoonId} webtoon={webtoon} />)}
     </VStack>
   );
 };
