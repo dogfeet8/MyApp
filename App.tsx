595de4e71541843e50e199dd99356bbaa66f274a
yichangmin <144650672+dogfeet8@users.noreply.github.com>
2024-02-15 02:11:22 +0900

    day-6 stack navigation 구현

--- App.tsx
+++ App.tsx
@@ -1,23 +1,41 @@
 import { config } from '@gluestack-ui/config';
-import { GluestackUIProvider, Center, SafeAreaView, ScrollView } from '@gluestack-ui/themed';
+import { GluestackUIProvider, SafeAreaView, StatusBar } from '@gluestack-ui/themed';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import WebToonSwiper from './components/Header/WebToonSwiper';
-import { Dimensions } from 'react-native';
-import WebtoonList from './components/List/WebtoonList';
+import ScreenOne from './screens/ScreenOne';
+import ScreenTwo from './screens/ScreenTwo';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ScreenThree from './screens/ScreenThree';
+import { ScreensParams } from './types';
 
 const queryClient = new QueryClient();
+const Stack = createStackNavigator<ScreensParams>();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <StatusBar barStyle='light-content' />
       <GluestackUIProvider config={config}>
         <SafeAreaView flex={1} bg='$backgroundDark950'>
-          <ScrollView flex={1} width='$full'>
-            <Center width={Dimensions.get('window').width} height={300} px='$5'>
-              <WebToonSwiper />
-            </Center>
-            <WebtoonList />
-          </ScrollView>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name='ScreenOne'
+                component={ScreenOne}
+                options={{ header: () => null }}
+              />
+              <Stack.Screen
+                name='ScreenTwo'
+                component={ScreenTwo}
+                options={{ header: () => null }}
+              />
+              <Stack.Screen
+                name='ScreenThree'
+                component={ScreenThree}
+                options={{ header: () => null }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
         </SafeAreaView>
       </GluestackUIProvider>
     </QueryClientProvider>
