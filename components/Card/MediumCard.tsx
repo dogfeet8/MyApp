50495869b97d9806b0f278aaef167a3a06cab007
yichangmin <144650672+dogfeet8@users.noreply.github.com>
2024-02-27 00:34:43 +0900

    터치 이슈 해결

--- components/Card/MediumCard.tsx
+++ components/Card/MediumCard.tsx
@@ -11,40 +11,37 @@ const MediumCard = ({ webtoon }: MediumCardProps) => {
   console.log(webtoon);
   return (
     <HStack borderWidth='$1' borderColor='$backgroundLight700'>
-      <Image
-        bg='$backgroundLight700'
-        alt={webtoon.title}
-        size='xl'
-        source={{
-          uri: webtoon.img,
-        }}
-      />
-      <Pressable
-        justifyContent='center'
-        pl='$5'
-        gap='$1'
-        onPress={() => navigation.navigate('Detail', { webtoon })}
-      >
-        <Text color='$blueGray50' size='md' bold={true}>
-          {webtoon.title}
-        </Text>
-        <Text color='$blueGray300' size='sm' bold={true}>
-          {webtoon.author}
-        </Text>
-        <HStack gap='$1'>
-          <Icon
-            as={StarIcon}
-            size='sm'
-            color='$yellow500'
-            fill='$yellow300'
-            mt='$0.5'
-            ml='$0.5'
-            $dark-color='$primary300'
-          />
-          <Text color='$blueGray300' size='sm' bold={true}>
-            {webtoon.fanCount}
+      <Pressable flexDirection='row' onPress={() => navigation.navigate('Detail', { webtoon })}>
+        <Image
+          bg='$backgroundLight700'
+          alt={webtoon.title}
+          size='xl'
+          source={{
+            uri: webtoon.img,
+          }}
+        />
+        <VStack justifyContent='center' pl='$5' gap='$1'>
+          <Text color='$blueGray50' size='md' bold={true}>
+            {webtoon.title}
           </Text>
-        </HStack>
+          <Text color='$blueGray300' size='sm' bold={true}>
+            {webtoon.author}
+          </Text>
+          <HStack gap='$1'>
+            <Icon
+              as={StarIcon}
+              size='sm'
+              color='$yellow500'
+              fill='$yellow300'
+              mt='$0.5'
+              ml='$0.5'
+              $dark-color='$primary300'
+            />
+            <Text color='$blueGray300' size='sm' bold={true}>
+              {webtoon.fanCount}
+            </Text>
+          </HStack>
+        </VStack>
       </Pressable>
     </HStack>
   );
